6e66c6de293e05d703a7b0f247d8e011cd5b9eda
SrimanUXmint <srimantharun@uxmint.in>
2024-03-14 16:39:04 +0530

    b1

--- backend/routes/order/orderRoutes.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const orderController = require("../../controllers/orders/OrdersControlller");
-
-router.get('/all?', orderController.get_user_orders);
-
-module.exports = router;
--- backend/routes/position/PositionRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const positioncriptController = require("../../controllers/position/positionController");
-
-router.get('/all?', positioncriptController.get_positions_by_userId);
-
-
-module.exports = router;
